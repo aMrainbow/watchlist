2a31aa409b4e1a12ea977ebabb19a89ccc767c19
M_B <2812248770@qq.com>
2023-08-06 12:50:09 +0800

    Add the base template and error template

--- app.py
+++ app.py
@@ -85,9 +85,22 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+
+@app.context_processor #模板上下文处理函数,
+                        #这个函数返回的变量（以字典键值对的形式）将会统一注入到每一个模板的上下文环境中，
+                        # 因此可以直接在模板中使用。
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    # user = User.query.first()
+    return render_template('404.html'), 404  # 返回模板和状态码
+
 @app.route('/')
 def index():
-    user = User.query.first()  # 读取用户记录
+    # user = User.query.first()  # 读取用户记录
     movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
